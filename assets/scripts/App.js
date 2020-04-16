ac0f3d2cb1016d6690d17d03cc7579695d8e6fd1
Christian Dimitrov <christian_dimitrov@abv.bg>
2020-04-16 08:33:05 +0300

    completed sticky header functionlaity

--- assets/scripts/App.js
+++ assets/scripts/App.js
@@ -1,7 +1,9 @@
 import MobileMenu from "./modules/mobileMenu";
 import RevealOnScroll from "./modules/revealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 import $ from "jquery";
 
 const mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+let stickyHeader = new StickyHeader();
